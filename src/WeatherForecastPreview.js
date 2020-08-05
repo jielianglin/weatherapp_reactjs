0133a8de000335ee788e5e69283487f746fa3318
gg__X <jielianglin821@gmail.com>
2020-08-05 20:39:32 +0200

    conditional rendering for forecast unit conversion

--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import WeatherForecastTime from "./WeatherForecastTime";
 
 export default function WeatherForecastPreview(props) {
+  const [unit, setUnit] = useState("celsius");
+
+  let celsiusTemperature = Math.round(props.data.main.temp);
+
   function getCelsiusTemperature() {
-    let celsiusTemperature = Math.round(props.data.main.temp);
     return `${celsiusTemperature}°C`;
   }
 
-  return (
-    <div className="ForecastCelsiusPreview col">
-      <WeatherForecastTime time={props.data.dt} />
+  function getFahrenheitTemperature() {
+    setUnit("fahrenheit");
+    let fahrenheitTemperature = Math.round((celsiusTemperature * 9) / 5 + 32);
+    return `${fahrenheitTemperature}°F`;
+  }
 
+  if (unit === "celsius") {
+    return (
+      <div className="ForecastCelsiusPreview col">
+        <WeatherForecastTime time={props.data.dt} />
+        <br />
+        <WeatherIcon code={props.data.weather[0].icon} />
+        <br />
+        {getCelsiusTemperature()}
+      </div>
+    );
+  } else {
+    <div className="ForecastFahrenheitPreview col">
+      <WeatherForecastTime time={props.data.dt} />
       <br />
       <WeatherIcon code={props.data.weather[0].icon} />
       <br />
-      {getCelsiusTemperature()}
-    </div>
-  );
+      {getFahrenheitTemperature()}
+    </div>;
+  }
 }
